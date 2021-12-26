e6858fe929b784454976be13fd58834d544d814e
Simon Vergauwen <nomisRev@users.noreply.github.com>
2021-12-26 19:26:32 +0100

    Change order gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,29 @@ plugins {
   alias(libs.plugins.kotlin.multiplatform)
   alias(libs.plugins.arrowGradleConfig.kotlin)
   alias(libs.plugins.arrowGradleConfig.formatter)
+  id("io.github.nomisrev.mpp-publish")
   alias(libs.plugins.dokka)
   alias(libs.plugins.kover)
-  id("io.github.nomisrev.mpp-publish")
+}
+
+tasks.withType<DokkaTask>().configureEach {
+  outputDirectory by rootDir.resolve("docs")
+  moduleName by "Saga"
+  dokkaSourceSets.apply {
+    named("commonMain") {
+      perPackageOption {
+        matchingRegex.set(".*\\.internal.*")
+        suppress by true
+      }
+      includes.from("README.md")
+      skipDeprecated by true
+      sourceLink {
+        localDirectory.set(project.file("src/commonMain/kotlin"))
+        remoteUrl.set(project.uri("https://github.com/nomisRev/Saga/tree/master/src/commonMain/kotlin").toURL())
+        remoteLineSuffix.set("#L")
+      }
+    }
+  }
 }
 
 kotlin {
@@ -83,24 +103,4 @@ tasks.withType<KotlinCompile>().configureEach {
 
 infix fun <T> Property<T>.by(value: T) {
   set(value)
-}
-
-tasks.withType<DokkaTask>().configureEach {
-  outputDirectory by rootDir.resolve("docs")
-  moduleName by "Saga"
-  dokkaSourceSets.apply {
-    named("commonMain") {
-      perPackageOption {
-        matchingRegex.set(".*\\.internal.*")
-        suppress by true
-      }
-      includes.from("README.md")
-      skipDeprecated by true
-      sourceLink {
-        localDirectory.set(project.file("src/commonMain/kotlin"))
-        remoteUrl.set(project.uri("https://github.com/nomisRev/Saga/tree/master/src/commonMain/kotlin").toURL())
-        remoteLineSuffix.set("#L")
-      }
-    }
-  }
 }
